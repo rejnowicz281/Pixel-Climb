d94373a43100969e4ecec334ca2ce152b7d3effb
Alan Rejnowicz <alanrejnowicz281@gmail.com>
2023-06-02 22:57:19 +0200

    Add animations for running, tweak existing animations

--- Player/player.gd
+++ Player/player.gd
@@ -8,47 +8,75 @@ var jumping = false
 var is_long_fall = false
 var jump_max = false
 var idle = false
+var running = false
+var turning_left = false
+var turning_right = false
+var rolling = false
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 func _physics_process(delta):
-	print(velocity)
-	if not is_on_floor():
-		velocity.y += gravity * delta
+	if not is_on_floor(): velocity.y += gravity * delta
 	
-	# Handle Jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-	
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	velocity.x = direction * SPEED
+	player_input()
 	
 	player_animation()
 	
 	move_and_slide()
+
+func player_input():
+	if not rolling:
+		var direction = Input.get_axis("ui_left", "ui_right")
+		velocity.x = direction * SPEED
+		if is_on_floor():
+			if Input.is_action_just_pressed("ui_up"): jump()
+
+			if Input.is_action_just_pressed("ui_accept"):
+				if velocity.x: roll()
+				else: jump()
+
+func jump():
+	velocity.y = JUMP_VELOCITY
+
+func roll():
+	print(velocity)
+	$AnimatedSprite2D.play("Roll")
+	rolling = true
+	await $AnimatedSprite2D.animation_finished
+	rolling = false
 
 func player_animation():
 	jumping = velocity.y <= -100
 	jump_max = velocity.y >- 100 and velocity.y < 0
 	if not is_long_fall: is_long_fall = velocity.y > 600
 	falling = velocity.y > 0
+	turning_left = velocity.x < 0
+	turning_right = velocity.x > 0
+	running = velocity.x and not velocity.y
+	idle = velocity == Vector2.ZERO and not is_long_fall and not $AnimatedSprite2D.animation == "Land"
 
-	if falling:
-		$AnimatedSprite2D.play("Fall")
-		print("Falling")
-	elif jumping:
-		$AnimatedSprite2D.play("Jump")
-		print("Jump")
-	elif jump_max:
-		$AnimatedSprite2D.play("Jump Max")
-		print("Max Jump")
-	elif is_long_fall and velocity.y == 0:
-		$AnimatedSprite2D.play("Land")
-		print("Big Landing")
-		await $AnimatedSprite2D.animation_finished
+	if turning_left:
+		$AnimatedSprite2D.flip_h = true
+	elif turning_right:
+		$AnimatedSprite2D.flip_h = false
+
+	if is_long_fall and velocity.y == 0:
+		if turning_left or turning_right:
+			roll()
+		else:
+			$AnimatedSprite2D.play("Land")
 		is_long_fall = false
-	else:
-		print("Idle")
-		$AnimatedSprite2D.play("Idle")
+
+	if not rolling:
+		if idle:
+			$AnimatedSprite2D.play("Idle")
+		elif running:
+			$AnimatedSprite2D.play("Run")
+		elif falling:
+			$AnimatedSprite2D.play("Fall")
+		elif jumping:
+			$AnimatedSprite2D.play("Jump")
+		elif jump_max:
+			$AnimatedSprite2D.play("Jump Max")
+
+	print($AnimatedSprite2D.animation)
